2a64002085ef91b57b959fc482050abed30bd9f0
Lucas Florio <lucasefe@gmail.com>
2023-02-02 15:22:31 -0300

    use newer voice-android version

--- src/android/com/phonegap/plugins/twiliovoice/TwilioVoicePlugin.java
+++ src/android/com/phonegap/plugins/twiliovoice/TwilioVoicePlugin.java
@@ -26,9 +26,6 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-// import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.installations.FirebaseInstallations;
-import com.google.firebase.installations.InstallationTokenResult;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.twilio.voice.Call;
 import com.twilio.voice.CallException;
